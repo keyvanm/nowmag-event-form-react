894ec7e189a5139a6a87f6c6dd628006cf4b589f
Keyvan Mosharraf <kayvanmsh@gmail.com>
2018-08-31 19:05:46 -0700

    Making zhe form

--- src/components/EventWizard/EventForm.js
+++ src/components/EventWizard/EventForm.js
@@ -1,7 +1,17 @@
 import React from 'react'
-import { Checkbox, Form } from 'semantic-ui-react'
+import { Checkbox, Form, Select, Input, TextArea, Dropdown, Divider, Radio, Popup, Header, Icon } from 'semantic-ui-react'
 
 import FormButtonGroup from './FormButtonGroup';
+
+import './EventForm.css'
+
+const categoryOptions = [
+  {
+    'text': "Art",
+    'key': 0, // pk?
+    'value': 0, // pk?
+  }
+]
 
 
 const FormExampleForm = ({
@@ -23,17 +33,52 @@ const FormExampleForm = ({
 
   return (
     <Form onKeyPress={onKeyPress} onSubmit={buttonHandlers.handleSubmit}>
-      <Form.Field>
-        <label>First Name</label>
-        <input placeholder='First Name' />
-      </Form.Field>
-      <Form.Field>
-        <label>Last Name</label>
-        <input placeholder='Last Name' />
-      </Form.Field>
-      <Form.Field>
-        <Checkbox label='I agree to the Terms and Conditions' />
-      </Form.Field>
+      {
+        currentStep === 1 &&
+        <div className="wizard-page">
+          <Form.Field>
+            <label>What is the title of your event?</label>
+            <Input autoFocus={true} placeholder='Title' />
+          </Form.Field>
+          <Form.Field>
+            <label>Which of these categories best describe your event?</label>
+            <Select placeholder='Category' options={categoryOptions} />
+          </Form.Field>
+          <Form.Field>
+            <label>Describe your event in a few sentences</label>
+            <TextArea autoHeight placeholder='Description'  />
+          </Form.Field>
+        </div>
+      }
+      {
+        currentStep === 2 &&
+        <div className="wizard-page">
+          <Form.Field>
+            <label>Where is your event located?</label>
+            <Dropdown autoFocus={true}  placeholder='Location' fluid search selection options={categoryOptions} />
+          </Form.Field>
+          <Divider horizontal>Or</Divider>
+          <Form.Field>
+            {/* <label>I couldn't find the venue on the above list</label> */}
+            <Radio toggle label="I couldn't find the venue on the above list" />
+          </Form.Field>
+          <Form.Field>
+            <label>Enter the location details manually</label>
+            <Input placeholder='Name (e.g. CN Tower)' />
+          </Form.Field>
+          <Form.Field>
+            <Popup
+              flowing
+              trigger={<Input placeholder='Address (e.g. 301 Front St W, Toronto, ON M5V 2T6)' />}
+              on='focus'
+            >
+              <Header color='yellow' icon='info' content="hint" as='h6' />
+              <p><Icon color='blue' name='location arrow' />Use this format</p>
+              <p><em>301 Front St W, Toronto, ON M5V 2T6</em></p>
+            </Popup>
+          </Form.Field>
+        </div>
+      }
 
       <FormButtonGroup loading={loading} buttonAbility={buttonAbility} buttonHandlers={buttonHandlers} />
 
