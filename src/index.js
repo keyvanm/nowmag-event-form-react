4e935a79d230999eee177c414a2d453766615482
Keyvan Mosharraf <kayvanmsh@gmail.com>
2018-09-11 16:05:18 -0400

    Using basename and homepage to prepare app for getting served from django

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/app">
       <App />
     </BrowserRouter>
   </Provider>,
